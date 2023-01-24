c4437a177a3c3d98f0013b180b3f31a5851a5c0d
Yelnur <yelnurzhumashev@gmail.com>
2023-01-24 18:00:51 +0600

    Выполнена задача №13

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -4,18 +4,73 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
 
-int maxDigit = MaxDigit(number);
-Console.WriteLine($"Третья число -> {maxDigit}");
 
-int MaxDigit(int num){
-    if (num > 99) 
-        while (num > 1000) 
-        {
-            num = num / 10;
-        }
-        int result = num % 10;
-    return result;   
+
+Console.Write("Введите число: "); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+if (num > 100){
+    while (num > 1000){
+        num = num / 10;
+    }
+
+    int res = num % 10; 
+    Console.WriteLine($"{num} -> {res}");
 }
+else 
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number < 0)
+//     number = (number < 0 ? -number : number);
+// if (number < 100)
+//     Console.WriteLine($"У числа {number} нет третьей цифры.");
+// int FindDigit(int digit)
+// {
+//     int result = 0;
+//     while (digit > 999)
+//         digit = digit / 10;
+//     result = digit % 10;
+//     return result;
+// }
+// Console.WriteLine(FindDigit(number));
+
+
+// Console.Write("Введите число: "); 
+// int num = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.WriteLine("Третья число -> "); 
+// if (num > 99)
+// {
+//     while (num > 1000)
+//     {
+//         num = num / 10;
+//         result = num % 10;
+//         Console.Write(result);
+//     }
+// }
+// else 
+// {
+//     Console.Write("третьей цифры нет");
+// }
+
+// int number = new Random().Next(-100000, 100000);
+// Console.WriteLine($"Получено число {number}");
+// if (number < 0)
+//     number = (number < 0 ? -number : number);
+// if (number < 100)
+//     Console.WriteLine($"У числа {number} нет третьей цифры.");
+// int FindDigit(int digit)
+// {
+//     int result = 0;
+//     while (digit > 999)
+//         digit = digit / 10;
+//     result = digit % 10;
+//     return result;
+// }
+// Console.WriteLine(FindDigit(number));
--- task_15/Program.cs
+++ task_15/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool Multiplicity(int number)
+{
+    return number > 5 && number < 8;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool res = Multiplicity(num);
+Console.WriteLine (res ? $"{num} -> Да": $"{num} -> Нет");
