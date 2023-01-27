76b9d1b318a8a8985157cdb757214b60eb9fc339
Yelnur <yelnurzhumashev@gmail.com>
2023-01-27 16:38:44 +0600

    Выполнена задача №18

--- task_18/Program.cs
+++ task_18/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (х и у).
+
+Console.WriteLine("Введите номер четверти");
+
+string quarter = Console.ReadLine();
+
+string result = RangeCoordinates(quarter);
+
+Console.WriteLine(result);
+
+string RangeCoordinates(string cords)
+{
+    if(cords == "1") return "x > 0, y > 0";
+    if(cords == "2") return "x < 0, y > 0";
+    if(cords == "3") return "x < 0, y < 0";
+    if(cords == "4") return "x > 0, y < 0";
+    return "Введите корректные данные";
+}
