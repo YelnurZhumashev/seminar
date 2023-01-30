7dfc9c15797bcf49c79de08d2ea9d4cede26a83b
Yelnur <yelnurzhumashev@gmail.com>
2023-01-30 15:40:17 +0600

    Добавлен задачи №30

---
+++ task_30/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] arr = CreateArray(8);
+PrintArray(arr);
+   
